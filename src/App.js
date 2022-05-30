a70cd510225ec99bfe31ed83f3958e56af5c97e6
farhan <farhanrafid97@gmail.com>
2022-05-30 13:44:24 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import React, { useEffect, useState } from 'react';
-
-import backgroundIcon from './backgroundIcon';
 import './App.css';
 import './bgIcon.css';
 
--- src/Background.js
+++ src/Background.js
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import backgroundIcon from './backgroundIcon';
 import iconData from './iconData';
