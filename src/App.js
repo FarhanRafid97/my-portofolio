c224b4ada930c3e6222b78a4ae5741f5525e667c
farhan <farhanrafid97@gmail.com>
2022-07-02 21:26:42 +0700

    learn framer-motion

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import './App.css';
 import './bgIcon.css';
+import Navbar from './components/Navbar/Navbar';
 
-import Background from './Background';
-import Home from './components/Home/Home';
-import Biodata from './components/biodata/Biodata';
 const App = () => {
   return (
     <>
       <div className="app">
-        <Background />
+        <Navbar />
+        {/* <Background />
         <Biodata />
-        <Home />
+        <Home /> */}
       </div>
     </>
   );
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <h3>Logo</h3>
+        <div
+          className="navigasiMenu {
+"
+        >
+          <p>TEst</p>
+          <p>TEst</p>
+          <p>TEst</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
